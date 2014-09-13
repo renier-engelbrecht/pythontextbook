a29df643fc4f0e2bad38957dbb201bbd6988c410
Renier Engelbrecht <renierengelbrecht@hotmail.com>
2014-09-13 08:55:50 +0200

    PythonTextbook
    PythonTextbook

---
+++ chapter 5 activity 9.py
@@ -0,0 +1,12 @@
+value = 1
+printvalue = '*'
+
+print (printvalue * value)
+value += 2
+print (printvalue * value)
+value += 2
+print (printvalue * value)
+value -= 2
+print (printvalue * value)
+value -= 2
+print (printvalue * value)
